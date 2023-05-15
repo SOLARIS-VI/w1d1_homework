3e385cd8b5fc4a8dea2df61a083dbca0c6095d04
PAUL DIMITRIS KYRIOU <paulkyriou@PAULs-MacBook-Pro.local>
2023-05-15 16:26:47 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,15 @@
+# Variables of different types
+name = "Paul"
+favorite_colour = "Red"
+age = 33
+
+# Calling methods/functions on variables
+print(name.upper())  # Convert name to uppercase
+
+# Using operators
+next_age = age + 1  # Addition
+half_age = age / 2  # Division
+
+print("Next age:", next_age)
+print("Half age:", half_age)
+
